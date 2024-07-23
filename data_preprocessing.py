bfe6319e68128525c9eb68963ecbb0778b107c95
Mattia Muraro <mattiamuraro8@gmail.com>
2024-07-23 18:09:20 +0200

    aggiunta la funzione per effettuare il data reduction

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,9 +16,32 @@ class DataPreprocessing:
     def transform_data(self) -> pd.DataFrame:
         return self.df
 
-    #tale funzione ha il compito di eliminare le colonne ridondanti del dataset
+    #tale funzione ha il compito di eliminare le colonne ridondanti del dataset. Il dataset è composto dalle colonne
+    # id_prenotazione, id_paziente, data_nascita, sesso, regione_residenza, codice_regione_residenza, asl_residenza, codice_asl_residenza,
+    # provincia_residenza, codice_provincia_residenza, comune_residenza, codice_comune_residenza, tipologia_servizio, descrizione_attivita, codice_descrizione_attivita,
+    # data_contatto, regione_erogazione, codice_regione_erogazione, asl_erogazione, codice_asl_erogazione, provincia_erogazione, codice_provincia_erogazione,
+    # struttura_erogazione, codice_struttura_erogazione, tipologia_struttura_erogazione, codice_tipologia_struttura_erogazione, id_professionista_sanitario,
+    # tipologia_professionista_sanitario, codice_tipologia_professionista_sanitario, data_erogazione, ora_inizio_erogazione, ora_fine_erogazione, data_disdetta
+
     def reduce_data(self) -> pd.DataFrame:
-        return self.df
+        #Eliminazione delle colonne ridondanti: le colonne ridondanti sono i vari codici che identificano le regioni, le asl, le province, i comuni, le attività, le tipologie di professionisti sanitari e le strutture
+        # viene eliminata la colonna id_paziente in quanto non è rilevante per il nostro scopo
+        # viene eliminata la colonna descrizione_attivita in quanto è ridondante con la colonna codice_descrizione_attivita
+        # viene eliminata la colonna tipologia_servizio in quanto si parla di Teleassistenza per ogni prestazione
+        ### data = self.df[['id_paziente', 'codice_regione_residenza', 'codice_asl_residenza', 'codice_provincia_residenza', 'codice_comune_residenza', 'tipologia_servizio', 'descrizione_attivita', 'codice_regione_erogazione', 'codice_asl_erogazione', 'codice_provincia_erogazione', 'codice_struttura_erogazione', 'codice_tipologia_struttura_erogazione', 'codice_tipologia_professionista_sanitario']]
+        
+        data = self.df[['id_prenotazione', 'data_nascita', 'sesso', 'regione_residenza', 'asl_residenza', 'provincia_residenza', 'comune_residenza', 'codice_descrizione_attivita', 'data_contatto', 'regione_erogazione', 'asl_erogazione', 'provincia_erogazione', 'struttura_erogazione', 'tipologia_struttura_erogazione', 'id_professionista_sanitario', 'tipologia_professionista_sanitario', 'data_erogazione', 'ora_inizio_erogazione', 'ora_fine_erogazione', 'data_disdetta']]
+
+        # le colonne rimanenti devono essere 'id_prenotazione', 'data_nascita', 'sesso', 'regione_residenza', 'asl_residenza', 'provincia_residenza', 'comune_residenza', 'codice_descrizione_attivita',
+        # 'data_contatto', 'regione_erogazione', 'asl_erogazione', 'provincia_erogazione', 'struttura_erogazione', 'tipologia_struttura_erogazione', 'id_professionista_sanitario',
+        # 'tipologia_professionista_sanitario', 'data_erogazione', 'ora_inizio_erogazione', 'ora_fine_erogazione', 'data_disdetta'
+
+
+        #tra tutte le colonne rimanenti si trattano come variabili categoriche tutte trann i campi data e ora e gli id
+        data = pd.get_dummies(data, columns=['sesso', 'regione_residenza', 'asl_residenza', 'provincia_residenza', 'comune_residenza', 'codice_descrizione_attivita', 
+                                               'regione_erogazione', 'asl_erogazione', 'provincia_erogazione', 'struttura_erogazione', 'tipologia_struttura_erogazione', 
+                                             'tipologia_professionista_sanitario'])
+        return data
 
     #Funzione che richiama tutte le istanze delle funzioni precedentemente create
     def preprocessing_data(self) -> pd.DataFrame:
