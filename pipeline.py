32aecccc7cfbcba1e10c2e9257e2ba241b545988
Edoardo Giacometto <giacomettoedoardo@gmail.com>
2024-07-23 17:01:05 +0200

    è stata modificata la classe DataPreprocessing

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,17 @@
 # aggiungere tutti gli import necessari per eseguire opgni step della pipeline
-
+import pandas as pd
 
 # Description: Classe che si occupa di eseguire tutti i passaggi del pipeline
 class Pipeline:
 
     # Costruttore della classe che si occuperà di eseguire tutti i passaggi del pipeline
-    def __init__(self, path):
+    def __init__(self, path ='data\challenge_campus_biomedico_2024_sample.csv'):
         self.path = path
+
+
+    #funzione per legge il dataset
+    def load_data(self) -> pd.DataFrame:
+        return pd.read_csv(self.path)
     
 
     # Metodo che esegue la pipeline
