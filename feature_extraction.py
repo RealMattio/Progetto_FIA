c62f54d206ec4995718308e684f1da1affe9ca70
Edoardo Giacometto <giacomettoedoardo@gmail.com>
2024-07-29 10:42:25 +0200

    Si sta la vorando alla classe FeatureExtraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,26 +6,34 @@ class FeatureExtraction:
         self.df = df
 
     def extract(self) -> pd.DataFrame:
-        print(self.df['data_erogazione'])
-        # Estraggo l'anno e il quadrimestre dalla data di erogazione
-        self.df['anno'] = self.df['data_erogazione'].dt.year
-
-        # Estraggo il mese dalla colonna di date, producendo un numero intero tra 1 e 12.
-        # riduco il valore del mese di 1. Questo perché i mesi sono numerati da 1 a 12, ma per il calcolo del quadrimestre è più conveniente lavorare con un indice che parte da 0.
-        # divido il valore ottenuto per 4 così da dividere l'anno in quadrimestri
-        #+ 1 converte l'indice zero-based ottenuto dalla divisione in un quadrimestre 1-based. 
         
-        self.df['quadrimestre'] = ((self.df['data_erogazione'].dt.month - 1) // 4) + 1
+        # Controllo iniziale dei dati
+        #print("Tipo di dato della colonna 'data_erogazione':", self.df['data_erogazione'].dtype)
+        #print("Valori NaT nella colonna 'data_erogazione':", self.df['data_erogazione'].isna().sum())
 
-        # Raggruppo i dati per anno e quadrimestre e conto il numero di teleassistenze
-        self.df = self.df.groupby(['anno', 'quadrimestre']).size().reset_index(name='numero_teleassistenze')
+        # Conversione in datetime
+        self.df['data_erogazione'] = pd.to_datetime(self.df['data_erogazione'], errors='coerce')
 
-        # Calcolo l'incremento tra i quadrimestri corrispondenti
-        self.df['incremento'] = self.df.groupby(['quadrimestre'])['numero_teleassistenze'].diff().fillna(0)
+        # Verifica se la conversione è avvenuta correttamente
+        if pd.api.types.is_datetime64_any_dtype(self.df['data_erogazione']):
+            self.df['anno'] = self.df['data_erogazione'].dt.year
+            self.df['quadrimestre'] = ((self.df['data_erogazione'].dt.month - 1) // 4) + 1
+            print(self.df)
+            #self.data = self.df
+            # Raggruppo i dati per anno e quadrimestre e conto il numero di teleassistenze
+            self.counts = self.df.groupby(['anno', 'quadrimestre']).size().reset_index(name='numero_teleassistenze')
 
-        # Discretizzo la variabile target 'incremento_teleassistenze'
-        bins = [-float('inf'), 0, 10, 50, float('inf')]
-        labels = ['Costant', 'Low', 'Medium', 'High']
-        self.df['incremento_teleassistenze'] = pd.cut(self.df['incremento'], bins=bins, labels=labels)
+            # Calcolo l'incremento tra i quadrimestri corrispondenti
+            self.counts['incremento'] = self.counts.groupby(['anno'])['numero_teleassistenze'].diff().fillna(0)
+            print(self.df)
+
+            # Discretizzo la variabile target 'incremento_teleassistenze'
+            bins = [-float('inf'), 0, 10, 50, float('inf')]
+            labels = ['Costant', 'Low', 'Medium', 'High']
+            self.counts['incremento_teleassistenze'] = pd.cut(self.counts['incremento'], bins=bins, labels=labels)
+
+            self.df = self.df.merge(self.counts, on=['anno', 'quadrimestre'], how='left')
+        else:
+            print("Errore: la colonna 'data_erogazione' non è di tipo datetime.")
 
         return self.df
