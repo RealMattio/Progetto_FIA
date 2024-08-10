bd8993f7a8a7b20693e9d031f689b5a4e7b729c4
Edoardo Giacometto <giacomettoedoardo@gmail.com>
2024-08-10 12:04:21 +0200

    Si sta implementando la PCA per la riduzione del dataset  e il calcolo della metrica finale

--- clustering.py
+++ clustering.py
@@ -185,3 +185,11 @@ class Clustering:
         clusters = km.fit_predict(X)
 
         self.data_categorical['Cluster_Kmodes'] = clusters
+
+    def clustering_PCA(self, n_components=2) -> pd.DataFrame:
+        X = self.data_categorical
+        self.n_components = n_components
+
+        pca = PCA(n_components=n_components)
+        X_pca = pca.fit_transform(X)
+        return pd.DataFrame(X_pca, columns=[f'PC{i+1}' for i in range(n_components)])
