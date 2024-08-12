52a48c4ccb71bce681d7089ff2d7a9662f18301f
Mattia Muraro <mattiamuraro8@gmail.com>
2024-08-12 15:05:56 +0200

    Corretta la pipeline e ultimato il calcolo della metrica finale

--- pipeline.py
+++ pipeline.py
@@ -9,16 +9,20 @@ from tqdm import tqdm
 import time
 import prince
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
+from datetime import datetime
 
 
-    # Funzione per ottenere tutte le combinazioni degli elementi di una lista
-def all_combinations(input_list):
+# Funzione per ottenere tutte le combinazioni degli elementi di una lista
+def all_combinations(input_list) -> list:
     result = []
     # Iteriamo su tutte le lunghezze possibili
     for r in range(1, len(input_list) + 1):
         # Otteniamo le combinazioni di lunghezza r
         combinations = itertools.combinations(input_list, r)
-        result.extend(combinations)
+        result.extend([list(comb) for comb in combinations])
+    for e in result:
+        list(e).append('incremento_teleassistenze')
     return result
 
 # Description: Classe che si occupa di eseguire tutti i passaggi del pipeline
@@ -36,6 +40,50 @@ class Pipeline:
         return pd.read_csv(self.path)
     
     def run(self):
+        print(f"Running pipeline from data in {self.path}")
+
+        # Fase 0: Lettura del file
+        print("Reading file")
+        data_preprocessing = dp.DataPreprocessing(self.data)
+        
+        # Fase 1: Data Preprocessing
+        print("Data preprocessing")
+        data = data_preprocessing.preprocessing_data()
+        
+        # Fase 2: Feature Extraction
+        print("Feature extraction")
+        feature_extractor = fe.FeatureExtraction(data)
+        data = feature_extractor.extract()
+
+        # Fase 3: Clustering
+        print("Clustering")
+        # Lista che contiene tutte le colonne con cui vorrò fare il clustering
+        lista_di_features=['asl_residenza', 'codice_descrizione_attivita', 'sesso', 'asl_erogazione', 'fascia_eta']
+        # Lista di tutte le combinazioni possibili delle features
+        features = all_combinations(lista_di_features)
+        risultati = []
+        n_cluster = self.n_cluster
+        df_cluster = data[lista_di_features]
+        clustering = cl.Clustering(df_cluster, n_cluster, clustering_model = self.clustering_type)
+        cluster_assigned = clustering.execute()
+        pd.concat([data, cluster_assigned], axis=1)
+
+        # Fase 4: Evaluation
+        print("Evaluation")
+        evaluation = ev.ClusteringEvaluation(df_cluster, data[['incremento_teleassistenze']], cluster_assigned, self.clustering_type)
+        data['Silhouette'] = evaluation.calculate_silhouette()
+        results = evaluation.evaluate()
+        results['features'] = lista_di_features
+        risultati.append(results)
+        
+        # Fase 5: Salvataggio dei risultati
+        print("Saving results")
+        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        name_file = f'test_results/test_results_{self.clustering_type}_{self.n_cluster}_{now}.csv'
+        pd.DataFrame(risultati).sort_values(by='purity', ascending=False).to_csv(name_file)
+
+    
+    def run2(self):
         print(f"Running pipeline from data in {self.path}")
         print("Reading file")
         data_preprocessing = dp.DataPreprocessing(self.data)
@@ -98,7 +146,7 @@ class Pipeline:
             # seleziono solo le colonne presenti in feature
             #feature.append('incremento_teleassistenze')
             data = dati[feature]
-            print(data)
+            #print(data)
             
             clustering = cl.Clustering(dati_dummy, n_cluster = n, data_categorical = data)
             #clustering = cl.Clustering(nuovi_dati, n_cluster = n, data_categorical = data)
@@ -120,16 +168,24 @@ class Pipeline:
                 data_clustered = clustering.data_categorical
             
             # EDO LAVORA QUA
+            '''
             # effettuare encoding con OneHot 
             encoder = OneHotEncoder(sparse_output=False)
             encoded_data = encoder.fit_transform(data_clustered)
             # poi passi a ClusteringEvaluation i dati binari
             #print(encoded_data)
+            '''
+            print(data_clustered.columns)
+            # stampo i valori unici per ogni colonna
+            print(data_clustered.nunique())
 
-            
+            encoded_data = pd.get_dummies(data_clustered).astype(float)
+
+            print(encoded_data.columns)
             # Fase 3: PCA
-            pca = prince.PCA(n_components=3)
-            pca_data = pca.fit_transform(pd.DataFrame(encoded_data))
+            n_components = 3
+            pca = PCA(n_components = n_components)
+            pca_data = pca.fit_transform(encoded_data)
             
             
             #in evaluation invece di data_clustered, gli va passato pca_data
@@ -153,120 +209,4 @@ class Pipeline:
 
 
 
-    
-    '''
-    # Metodo che esegue la pipeline
-    def run(self):
-        print(f"Running pipeline from data in {self.path}")
-        print("Reading file")
-        data_preprocessing = dp.DataPreprocessing(self.data)
-        print("Data preprocessing")
-        dati = data_preprocessing.preprocessing_data()
-
-        # ciclo per la selezione delle migliori features da utilizzare nel clustering
-    
-        #lista_di_features=['fascia_eta']
-        #lista_di_features=['asl_residenza', 'codice_descrizione_attivita', 'fascia_eta']
-        #lista_di_features=['asl_erogazione', 'fascia_eta']
-        lista_di_features=['asl_residenza', 'codice_descrizione_attivita', 'sesso', 'asl_erogazione', 'fascia_eta']
-        #lista_di_features=['regione_residenza', 'asl_residenza', 'codice_descrizione_attivita', 'sesso', 'tipologia_professionista_sanitario', 'regione_erogazione', 'asl_erogazione', 'fascia_eta']
-        
-        # lista di liste dove ogni lista interna è l'elenco delle features da utilizzare in quell'iterazione
-        features = all_combinations(lista_di_features)
-        #features = features.remove(('sesso',))
-        # Convertiamo le tuple in liste per una visualizzazione più chiara
-        features = [list(comb) for comb in features]
-        print(f"Number of combinations: {len(features)}")
-        #print(f"Features: {features}")
-        feature_extractor = fe.FeatureExtraction(dati)
-        dati = feature_extractor.extract()
-        #print(dati.columns)
-        #dati = dati[dati['anno'] != 2019]
-        #escludo il primo quadrimestre del 2019
-        dati = dati[~((dati['anno'] == 2019) & (dati['quadrimestre'] == 1))]
-
-
-        risultati = []
-        #features=[['asl_erogazione', 'fascia_eta']]
-        for feature in tqdm(features):
-            # selezioniamo le colonne del dataset il cui nome inizia con le feature scelte
-            #print(f"Features: {feature}")
-            data = pd.DataFrame()
-            for f in feature:
-                data1 = dati[[col for col in dati.columns if col.startswith(f)]]
-                data = pd.concat([data, data1], axis=1)
-            #print(data.head())
-            # feature extraction: in questa fase si calcolano le features 'incremento' e 'incremento_teleassistenze'
-            # vogliamo in ingresso un dataframe e in uscita verrà fornito lo stesso dataframe con le colonne 'incremento' e 'incremento_teleassistenze' aggiunte
-            data = pd.concat([data, dati[['data_erogazione', 'anno', 'quadrimestre', 'incremento_teleassistenze']]], axis=1)
-            #''
-            feature_extractor = fe.FeatureExtraction(data)
-            data = feature_extractor.extract()
-            print(data.shape)
-            data = data[data['anno'] != 2019]
-            print(data.shape)
-            ''
-            # clustering: in questa fase si esegue il clustering utilizzando le features appena selezionate
-            # vogliamo in ingresso un dataframe e restituisce lo stesso dataframe con N (numero di tipologie di cluster) colonne in più, una per ogni tipologia di cluster
-
-            # ATTENZIONE : quando viiene eseguito il clustering bisogna eliminare dal dataframe le colonne che non sono numeriche e rendere numeriche le colonne booleane
-            n = 4
-            #for n in tqdm(range(2, 10)):
-            clustering = cl.Clustering(data, n_cluster = n)
-            
-            clustering.clustering_kmeans()
-        
-            #print(f"Time Clustering KMeans: {time_k_means}")
-            #clustering.clustering_hierarchical()
-            #print(f"Clustering Hierarchical: {clustering.data['Cluster_HC'].unique()}")
-            #clustering.clustering_dbscan()
-            #print(f"Clustering DBSCAN: {clustering.data['Cluster_DBSCAN'].unique()}")
-            #clustering.clustering_expectationMaximisation()
-
-
-            ''
-            time_em_start = time.time()
-            clustering.clustering_expectationMaximisation()
-            time_em_end = time.time()
-            time_em = time_em_end - time_em_start
-            print(f"Time Clustering Expectation Maximisation: {time_em}")
-            ''
-            data = clustering.data
-
-            #label_counts = data['Cluster_HC'].value_counts()
-            #label_counts = data['Cluster_DBSCAN'].value_counts()
-            #label_counts = data['Cluster_EM'].value_counts()
-            #print("\nNumero di elementi per ogni cluster:")
-            #print(label_counts)
-
-            #print("\nNumero di elementi per ogni incremento:")
-            #print(label_counts)
-
-
-            # valutazione: in questa fase si valuta il clustering ottenuto e si salvano i risultati ottenuti
-            # vogliamo in ingresso un dataframe e restituisce un dizionario con i risultati del clustering
-            evaluation = ev.ClusteringEvaluation(data, 'incremento_teleassistenze', 'Cluster_Kmeans')
-            #evaluation = ev.ClusteringEvaluation(data, 'incremento_teleassistenze', 'Cluster_EM')
-            results = evaluation.eval()
-            results['features'] = feature
-            results['n_cluster'] = n
-
-            label_counts = data['Cluster_Kmeans'].value_counts()
-            results['cluster counts'] = label_counts
-            label_counts = data['incremento_teleassistenze'].value_counts()
-            results['label counts'] = label_counts
-
-            risultati.append(results)
-            ''
-            results = evaluation.evaluate()
-            results['features'] = feature
-            risultati.append(results[1])
-            ''
-        
-        #salvo i dati in un csv
-        pd.DataFrame(risultati).sort_values(by='purity', ascending=False).to_csv('test_results/test_results6.json')
-
-    '''
-
-
-
+   
